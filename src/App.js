e38c3303cf87574cba1ecfc68ad09fe0c92d13d8
Dominic Kimani <kymdommie@gmail.com>
2023-07-14 11:16:38 +0300

    few changes on app.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import './App.css';
 import Homepage from './Components/Homepage';
 
 function App() {
-  return (
-   
-    
-      <Router>
-        <Routes>
-        <Route path="/" element={<Homepage />} />
-        </Routes>
-    </Router>
-
-    
-  );
+    return (
+  <Router>
+    <Routes>
+        <Route> path="/" element={<Homepage/>}</Route>
+    </Routes>
+  </Router>
+    );
 }
 
 export default App;
